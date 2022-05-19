7a992e4d40aa5036df1319e631db74e727b411eb
Tommi <surfing@tommi.space>
2022-05-19 15:49:05 +0200

    updated external-links plugin

--- .eleventy.js
+++ .eleventy.js
@@ -104,7 +104,14 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(require('eleventy-plugin-find'));
 	eleventyConfig.addPlugin(require('@quasibit/eleventy-plugin-schema'));
 	eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'));
-	eleventyConfig.addPlugin(require('@sardine/eleventy-plugin-external-links'));
+	eleventyConfig.addPlugin(require('@aloskutov/eleventy-plugin-external-links'), {
+		url: 'https://tommi.space',
+		rel: ['noreferrer', 'noopener', 'external'],
+		overwrite: false,
+		excludedDomains:[
+			'https://tommasomarmo.com'
+		]
+	});
 	eleventyConfig.addPlugin(require('eleventy-plugin-embed-everything'), {
 		youtube: {
 			options: {
