e7bc8c84696cc40387c4ee567b4193932f8305a1
Tommi <surfing@tommi.space>
2023-03-18 09:23:35 +0100

    fix error when the page has no TOC

--- index.js
+++ index.js
@@ -27,13 +27,16 @@ function nowScroll() {
 }
 
 function tocBehavior() {
-	let footer = document.getElementsByTagName('footer')[0];
-	let footerHeight = footer.offsetHeight;
-	let tocHeight = pageHeight - footerHeight - 900; // adding some random pixels to consider margins etc.
-	if (document.body.scrollTop > tocHeight || document.documentElement.scrollTop > tocHeight) {
-		document.getElementById('toc').style.left = '-19vw';
-	} else {
-		document.getElementById('toc').style.left = '4vw';
+	let toc = document.getElementById('toc');
+	if (toc) {
+		let footer = document.getElementsByTagName('footer')[0];
+		let footerHeight = footer.offsetHeight;
+		let tocHeight = pageHeight - footerHeight - 900; // adding some random pixels to consider margins etc.
+		if (document.body.scrollTop > tocHeight || document.documentElement.scrollTop > tocHeight) {
+			toc.style.left = '-19vw';
+		} else {
+			toc.style.left = '4vw';
+		}
 	}
 }
 
