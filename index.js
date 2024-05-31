d831dd60316334bcb6597a69bb910d1357c42e13
Tommi <surfing@tommi.space>
2024-05-31 16:23:52 +0200

    🪄 Refactor using CSS Grid, still

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const htmlEl = document.documentElement;
+const bodyEl = document.body;
 const scrollBtn = document.getElementById('scrollBtn');
 const homeBtn = document.getElementById('tommiSpace');
 const animBtn = document.getElementById('animationToggleBtn');
 let animationStatus = localStorage.getItem('animationStatus') || 'running';
 
 function tommiSpaceAnimation() {
-	console.log(`animationStatus value inside function: ${animationStatus}`);
 	if (animationStatus == 'running' ) {
 		homeBtn.style.animationPlayState = 'running';
-		htmlEl.style.animationPlayState = 'running';
 		animBtn.title='Pause animations';
 		animBtn.innerHTML = '⏸️';
 	} else {
 		homeBtn.style.animationPlayState = 'paused';
-		htmlEl.style.animationPlayState = 'paused';
 		animBtn.title = 'Play animations';
 		animBtn.innerHTML = '▶️';
 	}
 	localStorage.setItem('animationStatus', animationStatus);
-	console.log(`animationStatus value at the end of function: ${animationStatus}`);
+}
+
+function changeBackground() {
+	if (backgroundStatus == 'wip') {
+		bodyEl.style.background = 'var(--background)';
+	} else {
+		bodyEl.style.background = 'unset';
+	}
 }
 
 function scrollBtnBehavior() {
@@ -34,20 +38,20 @@ function scrollBtnBehavior() {
 }
 
 function nowScroll() {
-	if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+	if (document.body.scrollTop > 1300) {
 		window.scrollTo(0, 0);
 	} else {
 		window.scrollTo(0, document.body.scrollHeight);
 	}
 }
 
-let Italian = document.querySelectorAll('.zibaldone a[hreflang=it], .grid a[hreflang=it]');
-let nonItalian = document.querySelectorAll('.zibaldone a[hreflang=en], .grid a[hreflang=en], .zibaldone a[hreflang=fr], .grid a[hreflang=fr]');
-let English = document.querySelectorAll('.zibaldone a[hreflang=en], .grid a[hreflang=en]');
-let nonEnglish = document.querySelectorAll('.zibaldone a[hreflang=it], .grid a[hreflang=it], .zibaldone a[hreflang=fr], .grid a[hreflang=fr]');
-let French = document.querySelectorAll('.zibaldone a[hreflang=fr], .grid a[hreflang=fr]');
-let nonFrench = document.querySelectorAll('.zibaldone a[hreflang=it], .grid a[hreflang=it], .zibaldone a[hreflang=en], .grid a[hreflang=en]');
-let allLang = document.querySelectorAll('.zibaldone a[hreflang=it], .grid a[hreflang=it], .zibaldone a[hreflang=en], .grid a[hreflang=en], .zibaldone a[hreflang=fr], .grid a[hreflang=fr]');
+let Italian = document.querySelectorAll('.list.entries a[hreflang=it], .grid a[hreflang=it]');
+let nonItalian = document.querySelectorAll('.list.entries a[hreflang=en], .grid a[hreflang=en], .list.entries a[hreflang=fr], .grid a[hreflang=fr]');
+let English = document.querySelectorAll('.list.entries a[hreflang=en], .grid a[hreflang=en]');
+let nonEnglish = document.querySelectorAll('.list.entries a[hreflang=it], .grid a[hreflang=it], .list.entries a[hreflang=fr], .grid a[hreflang=fr]');
+let French = document.querySelectorAll('.list.entries a[hreflang=fr], .grid a[hreflang=fr]');
+let nonFrench = document.querySelectorAll('.list.entries a[hreflang=it], .grid a[hreflang=it], .list.entries a[hreflang=en], .grid a[hreflang=en]');
+let allLang = document.querySelectorAll('.list.entries a[hreflang=it], .grid a[hreflang=it], .list.entries a[hreflang=en], .grid a[hreflang=en], .list.entries a[hreflang=fr], .grid a[hreflang=fr]');
 
 function toggleIt() {
 	for (let i = 0; i < nonItalian.length; i++) {
@@ -81,17 +85,14 @@ function toggleAll() {
 
 window.addEventListener('load', () => {	
 
-	console.log(`animationStatus value from localStorage: ${animationStatus}`);
 	tommiSpaceAnimation();
 	animBtn.addEventListener('click', () => {
-		console.log(`animationStatus value before click: ${animationStatus}`);
 		if (animationStatus == 'running') {
 			animationStatus = 'paused';
 		} else {
 			animationStatus = 'running';
 		}
 		tommiSpaceAnimation();
-		console.log(`animationStatus value after click: ${animationStatus}`);
 	});
 
 	if (document.querySelectorAll('[data-target-lang]')) {
