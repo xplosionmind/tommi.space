7b935751b9a79141aebc0a3e552202f2517690f5
Tommi <surfing@tommi.space>
2026-05-20 14:41:26 +0200

    ✨ Add guestbook! Closes #119
    Initial basic guestbook implementation, using
    https://codeberg.org/meadowingc/guestbooks and self-hosted on
    https://guestbook.tommi.space/

---
+++ data/guestbook.js
@@ -0,0 +1,11 @@
+import Fetch from '@11ty/eleventy-fetch';
+
+export default function () {
+	try {
+		const messages_api = 'https://guestbook.tommi.space/api/v2/get-guestbook-messages/1';
+		return Fetch(messages_api, {
+			duration: '1s',
+			type: 'json',
+		});
+	}
+};
